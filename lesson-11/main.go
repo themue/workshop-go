2ea97c813164425b40cbabf5ff2a7cd2fe5edfde
Frank Mueller <mail@themue.dev>
2020-05-20 22:57:26 +0200

    Extend switching lesson
    Signed-off-by: Frank Mueller <mail@themue.dev>

--- lesson-11/main.go
+++ lesson-11/main.go
@@ -35,7 +35,94 @@ func MultipleValuesSwitch() {
 	}
 }
 
+// DefaultValueSwitch shows switching with a default.
+func DefaultValueSwitch() {
+	x := 5
+	switch x {
+	case 1:
+		fmt.Println("default value switch: x is 1")
+	case 2:
+		fmt.Println("default value switch: x is 2")
+	case 3:
+		fmt.Println("default value switch: x is 3")
+	default:
+		fmt.Println("default value switch: x is something else (default at end)")
+	}
+
+	switch x {
+	case 1:
+		fmt.Println("default value switch: x is 1")
+	default:
+		fmt.Println("default value switch: x is something else (default not at end)")
+	case 2:
+		fmt.Println("default value switch: x is 2")
+	case 3:
+		fmt.Println("default value switch: x is 3")
+	}
+}
+
+// FallthroughValueSwitch shows switching with fallthroughs.
+func FallthroughValueSwitch() {
+	x := 2
+	switch x {
+	case 1:
+		fmt.Println("fallthrough value switch: x is 1")
+		fallthrough
+	case 2:
+		fmt.Println("fallthrough value switch: x is 2")
+		fallthrough
+	case 3:
+		fmt.Println("fallthrough value switch: x is 3?")
+		fallthrough
+	case 4:
+		fmt.Println("fallthrough value switch: x is 4?")
+		fallthrough
+	case 5:
+		fmt.Println("fallthrough value switch: x is 5?")
+	}
+}
+
+// ExpressionSwitch shows switching based on expressions.
+func ExpressionSwitch() {
+	x := 5
+	y := -5
+	switch {
+	case x < 0:
+		fmt.Println("expression switch: x is lower than 0")
+	case y > 0:
+		fmt.Println("expression switch: y is greater than 0")
+	case x == y:
+		fmt.Println("expression switch: x and y have the same value")
+	case x > 0 && y < 0:
+		fmt.Println("expression switch: x is greater than 0 and y lower than 0")
+	case x < 0 && y > 0:
+		fmt.Println("expression switch: x is lower than 0 and y greater than 0")
+	default:
+		fmt.Println("expression switch: I don't care")
+	}
+}
+
+// TypeSwitch shows switching based on the type of an interface.
+func TypeSwitch() {
+	var i interface{}
+	i = "guess what I am"
+	switch ti := i.(type) {
+	case int:
+		fmt.Println("type switch: i is an int:", ti)
+	case string:
+		fmt.Println("type switch: i is a string:", ti)
+	case bool:
+		fmt.Println("type switch: i is a bool:", ti)
+	default:
+		fmt.Println("type switch: I don't know")
+	}
+}
+
 func main() {
 	SingleValueSwitch()
 	MultipleValuesSwitch()
+	DefaultValueSwitch()
+	FallthroughValueSwitch()
+	ExpressionSwitch()
+	TypeSwitch()
 }
